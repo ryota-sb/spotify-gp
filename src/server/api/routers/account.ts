b11c118c1e2e01070089c0162de2ea54e4d13def
ryota-sb <ryota.sb.1206@gmail.com>
2023-05-17 21:39:18 +0900

    プロバイダーアカウントのIDを取得するプロシージャ定義

--- src/server/api/routers/account.ts
+++ src/server/api/routers/account.ts
@@ -3,14 +3,25 @@ import { protectedProcedure, createTRPCRouter } from "../trpc";
 export const accountRouter = createTRPCRouter({
   // accountテーブル トークン、リフレッシュトークン、トークン期限 取得
   getToken: protectedProcedure.query(async ({ ctx }) => {
-    const account = await ctx.prisma.account.findMany({
+    const accounts = await ctx.prisma.account.findMany({
       where: {
         userId: ctx.session.user.id,
       },
     });
-    const access_token = account[0]?.access_token;
-    const refresh_token = account[0]?.refresh_token;
-    const expired_at = account[0]?.expires_at;
+    const access_token = accounts[0]?.access_token;
+    const refresh_token = accounts[0]?.refresh_token;
+    const expired_at = accounts[0]?.expires_at;
     return { account: { access_token, refresh_token, expired_at } };
+  }),
+
+  // accountテーブル プロバイダーアカウントID取得
+  getProviderAccount: protectedProcedure.query(async ({ ctx }) => {
+    const accounts = await ctx.prisma.account.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+    const providerAccountId = accounts[0]?.providerAccountId;
+    return { account: { providerAccountId } };
   }),
 });
