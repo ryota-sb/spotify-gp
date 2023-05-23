f519d39dd777924a58a811065f836e7852ae54d0
ryota-sb <ryota.sb.1206@gmail.com>
2023-05-23 23:08:21 +0900

    ローティング画面作成

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -1,8 +1,8 @@
 const Loading = () => {
   return (
     <div className="flex h-screen items-center justify-center space-x-2">
-      <div className="h-3 w-3 animate-bounce rounded-full bg-green-600"></div>
-      <div className="h-3 w-3 animate-bounce rounded-full bg-green-600 animation-delay-200"></div>
+      <div className="h-3 w-3 animate-bounce rounded-full bg-white"></div>
+      <div className="h-3 w-3 animate-bounce rounded-full bg-white animation-delay-200"></div>
       <div className="h-3 w-3 animate-bounce rounded-full bg-green-600 animation-delay-400"></div>
     </div>
   );
